6f413b8a3b4dde013d81624c0419ce9fc7709b54
Xclusivep <110981085+Xclusivep@users.noreply.github.com>
2025-02-25 10:45:30 +0100

    Update settings.py

--- realestate/settings.py
+++ realestate/settings.py
@@ -82,12 +82,11 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'admin' ,
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST':'postgres-db',
-        'PORT': '5432',
-        
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT")
     }
 }
 
